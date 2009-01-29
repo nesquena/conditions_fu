50fa97f79a67d52fb9f5b85787bccf69363260b8
Jeff Smick <sprsquish@gmail.com>
2009-01-29 15:42:16 -0800

    move important stuff out of init.rb

--- lib/conditions_fu.rb
+++ lib/conditions_fu.rb
@@ -120,3 +120,20 @@ module ConditionsFu
     end
   end
 end
+
+require 'activerecord'
+
+Symbol.send(:include, ConditionsFu::SymbolQueryExtensions)
+
+# extend activerecord with the condition methods
+ActiveRecord::Base.send(:extend, ConditionsFu::Base)
+
+# override sanitize_sql_hash_for_conditions
+module ::ActiveRecord
+  class Base
+    class << self
+       # use the sanitize_sql_hash_for_conditions_with_attribute_queries
+       alias_method_chain :sanitize_sql_hash_for_conditions, :attribute_queries
+    end
+  end
+end
